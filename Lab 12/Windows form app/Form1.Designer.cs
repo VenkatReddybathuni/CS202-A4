528b9f4ff7ff6590ffc8c83b0148ed594d51d965
Venkat Reddybathuni <140537517+VenkatReddybathuni@users.noreply.github.com>
2025-04-17 11:12:43 +0530

    Add files via upload

---
+++ Lab 12/Windows form app/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AlarmWinFormsApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBoxTime = new TextBox();
+            buttonStart = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // textBoxTime
+            // 
+            textBoxTime.Location = new Point(30, 30);
+            textBoxTime.Name = "textBoxTime";
+            textBoxTime.Size = new Size(200, 27);
+            textBoxTime.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(250, 30);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(104, 27);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(438, 121);
+            Controls.Add(buttonStart);
+            Controls.Add(textBoxTime);
+            Name = "Form1";
+            Text = "Event-Driven Alarm App";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ Lab 12/Windows form app/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AlarmWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private DateTime targetTime;
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (DateTime.TryParse(textBoxTime.Text, out targetTime))
+            {
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("❌ Invalid time format. Please use HH:MM:SS.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Change background color
+            this.BackColor = Color.FromArgb(
+                rand.Next(256), rand.Next(256), rand.Next(256));
+
+            DateTime currentTime = DateTime.Now;
+
+            if (currentTime.Hour == targetTime.Hour &&
+                currentTime.Minute == targetTime.Minute &&
+                currentTime.Second == targetTime.Second)
+            {
+                timer1.Stop();
+                MessageBox.Show("⏰ Alarm! Time matched.");
+            }
+        }
+    }
+}
